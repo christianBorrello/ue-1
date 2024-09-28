6c3186d030beec971eb7a3f5b2a933ac761bd03b
christianBorrello <christian.borrello@live.it>
2024-09-28 23:06:14 +0200

    Creating BP_SlashCharacter and adding the initial functionalities

---
+++ Source/SlashV3/Private/Character/SlashCharacter.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/SlashCharacter.h"
+#include "Components/InputComponent.h"
+
+ASlashCharacter::ASlashCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ASlashCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASlashCharacter::MoveForward(float Value)
+{
+	if (Controller && (Value != 0.f)) 
+	{
+		FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, Value);
+	}
+}
+
+void ASlashCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(FName("MoveForward"), this, &ASlashCharacter::MoveForward);
+}
--- Source/SlashV3/Public/Pawns/Bird.h
+++ Source/SlashV3/Public/Pawns/Bird.h
@@ -42,9 +42,11 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+/*
 	void MoveForward(float Value);
 	void Turn(float Value);
 	void LookUp(float Value);
+*/
 
 private:
 	UPROPERTY(VisibleAnywhere)
